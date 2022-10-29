051876532c26ad44fbbf715b449332260927eed2
anttikotajarvi <jaypacket@gmail.com>
2022-10-29 20:06:04 +0300

    Generics.ts

--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,22 @@
-export type GenericMutable<T> = {
-  -readonly [P in keyof T]: T[any];
+"use strict";
+
+export type DeepReadonly<T> =
+    T extends (infer R)[] ? DeepReadonlyArray<R> :
+    T extends Function ? T :
+    T extends object ? DeepReadonlyObject<T> :
+    T;
+
+export interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
+
+export type DeepReadonlyObject<T> = {
+    readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
+
+
+export type Mutable<T> = {
+    -readonly [P in keyof T]: T[P];
+};
+export type GenericMutable<T> = {
+    -readonly [P in keyof T]: T[any];
+};
+
